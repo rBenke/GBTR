65c1c95c183fa05939ef25bc6e2e3163b00ec3e7
rBenke <robert.benke2@gmail.com>
2021-03-09 22:39:12 +0100

    Graph builder - component interfaces

---
+++ gbtr/graphBuilder/TF_IDF.py
@@ -0,0 +1,8 @@
+from CorpusFeature import CorpusFeatureInterface
+class TF_IDF(CorpusFeatureInterface):
+    def __init__(self):
+        pass
+
+    def get_feature(self, documents: list) -> list:
+        # TODO
+        return []
---
+++ gbtr/graphBuilder/TextGCNGraphBuilder.py
@@ -0,0 +1,19 @@
+from GraphBuilder import GraphBuilderInterface
+class TextGCNGraphBuilder(GraphBuilderInterface):
+    def __init__(self):
+        pass
+
+    def build_graph(self, documents: list) -> list:
+        # TODO
+        return []
+
+    def build_adjacency_matrix(self) -> list:
+        # TODO
+        return []
+
+    def build_feature_matrix(self) -> list:
+        # TODO
+        return []
+
+
+
